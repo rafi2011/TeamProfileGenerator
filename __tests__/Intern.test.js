7cba25f8fd1d6a626863f4f253b7f902fa3160ed
rafi2011 <rnessa2011@gmail.com>
2021-08-16 23:18:23 -0400

    added more codes in tests: intern and managers tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -11,3 +11,60 @@ describe("Intern Class Test", () => {
       expect(internObj instanceof Intern).toEqual(true);
     });
   })
+
+  describe("Interns ID Test", () => {
+    // test initialization
+    test("testing to get the right id", () => {
+      // arrange, act
+      const internObj = new Intern(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(internObj.getId()).toEqual(8);
+    });
+  });
+
+  describe("Intern's Name Test", () => {
+    // test initialization
+    test("testing to get the right name", () => {
+      // arrange, act
+      const internObj = new Intern(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(internObj.getName()).toEqual("Nessa");
+    });
+  });
+
+  describe("Intern's email Test", () => {
+    // test initialization
+    test("testing to get the right email", () => {
+      // arrange, act
+      const internObj = new Intern(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(internObj.getEmail()).toEqual("nessa@gmail.com");
+    });
+  });
+
+  describe("Intern's school Test", () => {
+    // test initialization
+    test("testing to get the right school", () => {
+      // arrange, act
+      const internObj = new Intern(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(internObj.getSchool()).toEqual("nessa123");
+    });
+  });
+
+  describe("Intern's role Test", () => {
+    // test initialization
+    test("testing to get the right role", () => {
+      // arrange, act
+      const internObj = new Intern(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(internObj.getRole()).toEqual("Intern");
+    });
+  });
+
+
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,67 @@
+const Manager = require("../lib/Manager");
+
+describe("Manager's Class Test", () => {
+    // test initialization
+    test("Test for the right class", () => {
+      // arrange, act
+      const managerObj = new Manager(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(managerObj instanceof Manager).toEqual(true);
+    });
+  })
+
+  describe("Manager's ID Test", () => {
+    // test initialization
+    test("testing to get the right id", () => {
+      // arrange, act
+      const managerObj = new Manager(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(managerObj.getId()).toEqual(8);
+    });
+  });
+
+  describe("Manager's Name Test", () => {
+    // test initialization
+    test("testing to get the right name", () => {
+      // arrange, act
+      const managerObj = new Manager(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(ManagerObj.getName()).toEqual("Nessa");
+    });
+  });
+  describe("Manager's email Test", () => {
+    // test initialization
+    test("testing to get the right email", () => {
+      // arrange, act
+      const managerObj = new Manager(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(managerObj.getEmail()).toEqual("nessa@gmail.com");
+    });
+  });
+
+  describe("Manager's office number Test", () => {
+    // test initialization
+    test("testing to get the right office number", () => {
+      // arrange, act
+      const managerObj = new Manager(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(managerObj.getEmail()).toEqual("nessa@gmail.com");
+    });
+  });
+
+  describe("Manager's role Test", () => {
+    // test initialization
+    test("testing to get the right role", () => {
+      // arrange, act
+      const managerObj = new Manager(6, "Nessa", "nessa@gmail.com", "nessa123");
+  
+      // assert
+      expect(managerObj.getRole()).toEqual("Intern");
+    });
+  });
+
